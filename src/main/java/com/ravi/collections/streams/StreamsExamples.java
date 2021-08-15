c1f80836e174b5525ba18a4e78fae2fbc5da5383
ravinder <ravinder.reddy54@gmail.com>
2021-08-15 09:19:29 -0500

    Add more examples

--- src/main/java/com/ravi/collections/streams/StreamsExamples.java
+++ src/main/java/com/ravi/collections/streams/StreamsExamples.java
@@ -2,10 +2,19 @@ package com.ravi.collections.streams;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsExamples {
 
     public static void main(String[] args) {
+
+//        Supplier<String> i = () -> "Car";
+//        Consumer<String> c = x -> System.out.println(x.toLowerCase());
+//        Consumer<String> d = x -> System.out.println(x.toUpperCase());
+//        c.andThen(d).accept(i.get());
+//        System.out.println();
+
+
         List<Integer> list = Arrays.asList(1,2,3,4,5);
 
         //find min value in list
@@ -17,6 +26,9 @@ public class StreamsExamples {
         System.out.println(evenNumbers.toString());
         //filter and get count
         System.out.println(list.stream().filter(i -> i%2==0).count());
+
+        //prints yellow
+        System.out.println(Stream.of("green","yellow","blue").max((s1,s2) -> s1.compareTo(s2)).filter(s -> s.endsWith("n")).orElse("yellow"));
 
     }
 
